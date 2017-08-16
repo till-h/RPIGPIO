5275ab88c09af91dfcbac9b66ef880e8fc4d61ca
Till Hackler <till.hackler@gmx.com>
2017-08-16 02:13:38 +0100

    Add SD card cloning script

--- CopySDCard.sh
+++ CopySDCard.sh
@@ -3,7 +3,7 @@ df -h
 read -p "Do you want to continue? Will overwrite anything on /dev/mmcblk0. (Enter to continue, Strg+C to abort)" yn
 umount /dev/mmcblk0*
 echo "Clearing SD card..."
-dd bs=4M if=/dev/null of=/dev/mmcblk0 status=progress
+dd bs=4M if=/dev/zero of=/dev/mmcblk0 status=progress
 echo "Sync..."
 sync
 echo "Writing image to SD card..."
@@ -11,3 +11,4 @@ dd bs=4M if=/home/till/RaspbianJesseGPIO.img of=/dev/mmcblk0 status=progress
 echo "Sync..."
 sync
 echo "Done."
+paplay Done.ogg
